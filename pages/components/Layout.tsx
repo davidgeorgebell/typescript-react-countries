2e12130b52ee1453b6aa2882e419ead6c456e988
davidgeorgebell <bell.dave93@gmail.com>
2020-11-25 08:34:30 +0800

    Bug fixes

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 
 type LayoutProps = {
   title: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 const Layout = ({ title, children }: LayoutProps) => {
