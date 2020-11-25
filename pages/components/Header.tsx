0ef279f43063af9efde66b366925babf631a9074
davidgeorgebell <bell.dave93@gmail.com>
2020-11-25 08:07:31 +0800

    Framer motion animations

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ handleSearchFilter }) => {
   return (
-    <header className='flex bg-white border-b border-gray-200 fixed top-0 inset-x-0 z-100 h-24 items-center'>
+    <header className='flex bg-white border-b border-gray-200  top-0 inset-x-0 z-100 h-24 items-center'>
       <form className='mx-auto container max-w-2xl px-4'>
         <input
           className='w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-24'
